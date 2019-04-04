dca819e80a0d52601e94392fc89e11c1ad9d7907
kxnwac <lcahyu@yahoo.com>
2019-04-04 15:14:33 +0700

    commit

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.asus.a10116502_akb11_kenwite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
